04f2035c936aa8816af9b99c8cf41fc020e437ad
LukaKveladze <Kveladze.luka@kiu.edu.ge>
2024-12-27 22:37:07 +0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+    public static <T> void main(String[] args){
+        System.out.println(Reader.input());
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    // Method to create a variable and assign a value
+    public static <T> T createVariable (String type, T value){
+        // In this case, we simply return the value
+        return value;
+    }
+}
